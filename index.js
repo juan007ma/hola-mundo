4c9bd30d6da25222754d924ab0122df8cdd06540
juan007ma <47800671+juan007ma@users.noreply.github.com>
2019-02-20 18:25:43 +0100

    pract 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express()
 
-app.get('/',(req,res) => res.send("hola mundo"))
+app.get('/',(req,res) => res.send("ya ttengo hambre x3"))
 app.listen(3000, () => console.log('el servidor se ejecuta en http://localhost:3000'))
