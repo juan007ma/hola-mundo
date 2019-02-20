b42fa5fbba5036690d9744dbbe1869b29f1cec59
juan007ma <47800671+juan007ma@users.noreply.github.com>
2019-02-20 18:20:14 +0100

    change
    empesar

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express()
+
+app.get('/',(req,res) => res.send("hola mundo"))
+app.listen(3000, () => console.log('el servidor se ejecuta en http://localhost:3000'))
